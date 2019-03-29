88496d070c4dbd637d0e4caab20ef1c0e91d4146
GitHubUPB <47457742+GitHubUPB@users.noreply.github.com>
2019-03-29 14:33:10 -0500

    Segundo

--- segundo.py
+++ segundo.py
@@ -1,21 +1,21 @@
+def a_power_b (a,b):
+    prod=1
+    for x in range (0,b):
         prod=prod*a
-        return prod
+    return prod
 
 
-bas=int(input("ingrese la base"))
-exp=int(input("ingrese el exponente"))
+#codigo
 
-result=a_power_b(bas,exp)
 while True:
-
-    base=int(input("ingrese la base"))
+    
+    bas=int(input("ingrese la base"))
     if bas ==0:
-        print("numero es cero")
+        print("numero es cero ####")
         break
     exp=int(input("ingrese el exponente"))
     result=a_power_b(bas,exp)
     print("el resultado de su potencia es: ",result)
-    if bas ==0:
+    if base ==0:
         break
 
-print("el resultado de su potencia es: ",result)
