4df2e6704146afb1bd96abb1e1057f293b7aa07f
Maicol Bernal <maicol.bernal.2019@upb.edu.co>
2019-03-29 13:49:13 -0500

    Correccion De ERRORES

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-    prod=prod*a
+def a_power_b (a,b):
+    prod=1
+    for x in range (0,b):
+        prod=prod*a
         return prod
-
-
-
-bas=int(input("ingresar la base"))
-exp=int(input("ingresar el exponente"))
-
-
-result=a_power_b(bas,exp)
-while True:
-
-    bas=int(input("ingresar la base"))
-    if bas ==0:
-        print("numero es cero")
-        break
-    exp=int(input("ingresar el exponente"))
-    result=a_power_b(bas,exp)
-    print("el resultado de su potencia es: ",result)
-    if bas ==0:
-        break
-
-print("el resultado de su potencia es: ",result)
+    for x in range (1,b):
+        prod=prod*b
+    return prod
