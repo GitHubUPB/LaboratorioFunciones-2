6868b4cdaa53ee0acb52e505a12a6701d4adeb2b
GitHubUPB <47457742+GitHubUPB@users.noreply.github.com>
2019-03-29 14:34:20 -0500

    Cuarto

--- Cuarto.py
+++ Cuarto.py
@@ -2,13 +2,11 @@ def a_power_b (a,b):
     prod=1
     for x in range (1,b+1):
         if x>=1000:
-            raise StopIteration("Pailas")
+            raise StopIteration("No Se Pudo")
         
         prod=prod*a
     return prod
 
-
-#codigo
 potencia=0
 par=0
 impar=0
@@ -16,17 +14,13 @@ error=0
 
 while True:
 
-    
-    
-
-    
     try:
         potencia=potencia+1
-        base=int(input("ingrese la base"))
-        if base ==0:
+        bas=int(input("ingrese la base"))
+        if bas ==0:
             print("numero es cero ####")
             break
-        exponente=int(input("ingrese el exponente"))
+        exp=int(input("ingrese el exponente"))
         result=a_power_b(base,exponente)
         print("el resultado de su potencia es: ",result)
         if result % 2 ==0:
