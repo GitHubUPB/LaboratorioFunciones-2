4906798a4607a20b25739eef58a0c4a782c63e0d
Maicol Bernal <maicol.bernal.2019@upb.edu.co>
2019-03-29 13:54:35 -0500

    tercero

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
+
 def a_power_b (a,b):
     prod=1
-    for x in range (0,b):
-        prod=prod*a
-        return prod
     for x in range (1,b):
         prod=prod*b
+    for x in range (1,b+1):
+        if x>=1000:
+            raise StopIteration("No Se Pudo")
+
+        prod=prod*a
     return prod
+
+
+
+while True:
+
+    bas=int(input("ingresar la base"))
+    if bas ==0:
+        print("numero es cero")
+        break
+    
+    exp=int(input("ingresar el exponente"))
+    result=a_power_b(bas,exp)
+    print("el resultado de su potencia es: ",result)
+    if bas ==0:
+        break
+    try:
+        bas=int(input("ingresar la base"))
+        if bas ==0:
+            print("numero es cero")
+            break
+        exp=int(input("ingresar el exponente"))
+    except ValueError:
+        print("ingreso una letra")
+    except StopIteration:
+        print("paso el limite")
