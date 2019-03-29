79a55bcc0c7aae2ae86dcbda04fe17341479dad7
Maicol Bernal <maicol.bernal.2019@upb.edu.co>
2019-03-29 13:38:26 -0500

    Primero

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def a_power_b (a,b):
+    prod=1
+    for x in range (0,b):
+        prod=prod*a
+        return prod
+
+
+
+bas=int(input("ingrese El Valor Para La Base"))
+exp=int(input("ingrese El Valor Para La Exponencia"))
+
+result=a_power_b(bas,exp)
+
+print("el resultado de su potencia es: ",result)
