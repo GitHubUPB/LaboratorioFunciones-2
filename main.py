2ebd1e6cca1706419c383b09cdd4be5c768b2976
Maicol Bernal <maicol.bernal.2019@upb.edu.co>
2019-03-29 13:47:45 -0500

    segundo

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-
-def a_power_b (a,b):
-    prod=1
-    for x in range (0,b):
-        prod=prod*a
+    prod=prod*a
         return prod
 
 
 
-bas=int(input("ingrese El Valor Para La Base"))
-exp=int(input("ingrese El Valor Para La Exponencia"))
+bas=int(input("ingresar la base"))
+exp=int(input("ingresar el exponente"))
+
 
 result=a_power_b(bas,exp)
+while True:
+
+    bas=int(input("ingresar la base"))
+    if bas ==0:
+        print("numero es cero")
+        break
+    exp=int(input("ingresar el exponente"))
+    result=a_power_b(bas,exp)
+    print("el resultado de su potencia es: ",result)
+    if bas ==0:
+        break
 
 print("el resultado de su potencia es: ",result)
