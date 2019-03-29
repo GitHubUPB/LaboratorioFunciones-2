b6f76262759ad46abf5f477bf0149df608a993fc
GitHubUPB <47457742+GitHubUPB@users.noreply.github.com>
2019-03-29 14:31:09 -0500

    Cuarto

--- Cuarto.py
+++ Cuarto.py
@@ -1,8 +1,47 @@
-            print("numero es cero")
+def a_power_b (a,b):
+    prod=1
+    for x in range (1,b+1):
+        if x>=1000:
+            raise StopIteration("Pailas")
+        
+        prod=prod*a
+    return prod
+
+
+#codigo
+potencia=0
+par=0
+impar=0
+error=0
+
+while True:
+
+    
+    
+
+    
+    try:
+        potencia=potencia+1
+        base=int(input("ingrese la base"))
+        if base ==0:
+            print("numero es cero ####")
             break
-        exp=int(input("ingrese el exponente"))
-        result=a_power_b(bas,exp)
+        exponente=int(input("ingrese el exponente"))
+        result=a_power_b(base,exponente)
         print("el resultado de su potencia es: ",result)
+        if result % 2 ==0:
+            par=par+1
+        else:
+            impar=impar+1
     except ValueError:
+        error=error+1
         print("ingreso una letra")
-    except StopIteration:    
+    except StopIteration:
+        error=error+1
+        print("sobrepaso el limite")
+
+
+print("las potencias que intento calcular  son: ",potencia)
+print("los resultados pares son: ",par)
+print("los resultados impares son: ",impar)
+print("la cantidad de errores son: ",error)
